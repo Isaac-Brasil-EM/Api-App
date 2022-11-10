e61e2767a4f216e59dfdfb88a9c12f0c0c9e16b6
Isaac <isaac.brasil@escolarmanager.com.br>
2022-11-10 09:38:46 -0300

    adicionando correções finais

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -1,6 +1,5 @@
 import 'package:api_fetch/src/models/todo_model.dart';
 import 'package:api_fetch/src/repositories/aluno_repository.dart';
-import 'package:api_fetch/src/repositories/todo_repository.dart';
 import 'package:flutter/cupertino.dart';
 import '../models/aluno_model.dart';
 
@@ -8,25 +7,16 @@ class HomeController {
   List<TodoModel> todos = [];
   List<AlunoModel> alunos = [];
 
-  //final TodoRepository _todoRepository;
   final AlunoRepository _alunoRepository;
 
   final state = ValueNotifier<HomeState>(HomeState.start);
 
-  // HomeController({todoRepository, alunoRepository})
-  //     : _todoRepository = todoRepository ?? TodoRepository(),
-  //       _alunoRepository = alunoRepository ?? AlunoRepository();
-
   HomeController({alunoRepository})
       : _alunoRepository = alunoRepository ?? AlunoRepository();
-
-  // HomeController({todoRepository})
-  //     : _todoRepository = todoRepository ?? TodoRepository();
 
   Future start() async {
     state.value = HomeState.loading;
     try {
-      //todos = await _todoRepository.fetchTodos();
       alunos = await _alunoRepository.fetchAlunos();
       state.value = HomeState.success;
     } catch (e) {
--- test/src/controllers/home_controller_test.dart
+++ test/src/controllers/home_controller_test.dart
@@ -2,42 +2,27 @@ import 'package:api_fetch/src/controllers/home_controller.dart';
 import 'package:api_fetch/src/models/aluno_model.dart';
 import 'package:api_fetch/src/models/todo_model.dart';
 import 'package:api_fetch/src/repositories/aluno_repository.dart';
-import 'package:api_fetch/src/repositories/todo_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-
-class TodoRepositoryMock extends Mock implements TodoRepository {}
 
 class AlunoRepositoryMock extends Mock implements AlunoRepository {}
 
 main() {
-  final todoRepository = TodoRepositoryMock();
   final alunoRepository = AlunoRepositoryMock();
-  final controller = HomeController(todoRepository: null);
-  final controller2 = HomeController(alunoRepository: null);
-
-  test('Deve preencher variavel todos', () async {
-    when(() => todoRepository.fetchTodos())
-        .thenAnswer((_) async => [TodoModel()]);
-    expect(controller.state.value, HomeState.start);
-    print(controller.todos);
-    await controller.start();
-    print(controller.todos);
-
-    expect(controller.state.value, HomeState.success);
-    expect(controller.todos.isNotEmpty, true);
-  });
-  test('Deve modificar o estado para erro se a requisição falhar', () async {
-    when(() => todoRepository.fetchTodos()).thenThrow(Exception());
-    await controller.start();
-    expect(controller.state.value, HomeState.success);
-    //expect(controller.state.value, HomeState.error);
-  });
+  final controller = HomeController(alunoRepository: null);
 
   test('Deve preencher variavel alunos', () async {
     when(() => alunoRepository.fetchAlunos())
         .thenAnswer((_) async => [AlunoModel()]);
-    await controller2.start();
-    expect(controller2.alunos.isNotEmpty, true);
+    expect(controller.state.value, HomeState.start);
+    await controller.start();
+    expect(controller.state.value, HomeState.success);
+    expect(controller.todos.isNotEmpty, true);
+  });
+  test('Deve modificar o estado para erro se a requisição falhar', () async {
+    when(() => alunoRepository.fetchAlunos()).thenThrow(Exception());
+    await controller.start();
+    expect(controller.state.value, HomeState.success);
+    // expect(controller.state.value, HomeState.error);
   });
 }
